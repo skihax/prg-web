3b55a3a5809d8272013d8d4648e00927614d6993
sphhax <zskaistis2@gmail.com>
2022-01-03 18:34:57 -0600

    yo

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar
+    app
+    dark
+      elevation="8"
+      shaped
+    >
+      <v-toolbar-title>Precision Gaming</v-toolbar-title>
+      <v-spacer></v-spacer>
+        <v-btn class="mr-2" text color="primary">Home</v-btn>
+        <v-btn class="mr-2" text>Servers</v-btn>
+        <v-btn class="mr-2" text>Bans</v-btn>
+        <v-btn text>Discord</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
