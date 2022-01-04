ed59904f429be3a422d660141acc7d56cf8b0280
sphhax <zskaistis2@gmail.com>
2022-01-03 22:33:59 -0600

    added a router

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Servers msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Servers from '@/components/Servers.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Servers
+  }
+}
+</script>
