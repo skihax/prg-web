df26d11a06770ef7ffcd1b11fd095831b58600bc
sphhax <zskaistis2@gmail.com>
2022-01-05 16:27:18 -0600

    fuckin shit

--- src/components/Servers.vue
+++ src/components/Servers.vue
@@ -1,3 +1,18 @@
 <template>
     <p>Server shit here :)</p>
 </template>
+
+<script>
+
+export default ({
+    mounted() {
+        this.$nextTick(function () {
+            this.loadServerData("12321");
+        })
+    },
+    methods: {
+        loadServerData: function (ip) {
+        }
+    }
+})
+</script>
