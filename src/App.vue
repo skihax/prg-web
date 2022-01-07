7b865a70d420729193239f1b1f9cfacd8fe88bf0
sphhax <zskaistis2@gmail.com>
2022-01-06 19:10:08 -0600

    STUFF

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,36 @@
 <template>
-  <v-app>
+  <v-app id="main" :style="{background: $vuetify.theme.themes[theme].background}">
     <v-app-bar
     app
-    dark
-      elevation="8"
-      shaped
+    color="secondary"
+    elevation="8"
+    class="rounded-b-xl"
     >
-      <v-toolbar-title>Precision Gaming</v-toolbar-title>
+      <v-toolbar-title>
+        <v-img
+          max-width="120"
+          src="@/assets/logo.png"
+        ></v-img>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
-        <v-btn class="mr-2" text color="primary">Home</v-btn>
-        <v-btn class="mr-2" text>Info</v-btn>
-        <v-btn class="mr-2" text>Bans</v-btn>
-        <v-btn text>Discord</v-btn>
+        <v-btn class="mr-2 rounded-xl" text>Home</v-btn>
+        <v-btn class="mr-2 rounded-xl" text>Info</v-btn>
+        <v-btn class="mr-2 rounded-xl" text href="https://www.prgaming.net/bans" target="_blank">Bans</v-btn>
+        <v-btn class="rounded-xl" text>Discord</v-btn>
     </v-app-bar>
     <v-main>
       <router-view/>
     </v-main>
   </v-app>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  computed:{
+    theme(){
+      return (this.$vuetify.theme.dark) ? 'dark' : 'light'
+    }
+  }
+};
+</script>
--- src/components/Servers.vue
+++ src/components/Servers.vue
@@ -1,10 +1,89 @@
 <template>
-    <p>Server shit here :)</p>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-card
+                elevation="8"
+                dark
+                class="rounded-xl"
+                >
+                    <v-card-title>Deagle</v-card-title>
+                    <v-card-text>
+                        <v-row>
+                            <v-col>
+                                <span class="overline">Status</span><br />
+                                <v-icon large color="info">mdi-circle-small</v-icon> Online
+                            </v-col>
+                            <v-col>
+                                <span class="overline">Location</span><br />
+                                <v-icon large color="info">mdi-circle-small</v-icon> East (Virginia)
+                            </v-col>
+                            <v-col>
+                                <span class="overline">Current Map</span><br />
+                                <v-icon large color="info">mdi-circle-small</v-icon> am_junkyard
+                            </v-col>
+                            <v-col>
+                                <span class="overline">Online Players</span><br />
+                                <v-icon large color="info">mdi-circle-small</v-icon> 24/30
+                            </v-col>
+                        </v-row>
+                          <v-sparkline
+                            :value="value"
+                            :gradient="gradient"
+                            :smooth="radius || false"
+                            :padding="padding"
+                            :line-width="width"
+                            :stroke-linecap="lineCap"
+                            :gradient-direction="gradientDirection"
+                            :fill="fill"
+                            :type="type"
+                            :auto-line-width="autoLineWidth"
+                            auto-draw
+                            height="50%"
+                        ></v-sparkline>
+                        24 Players Online
+                        <v-divider></v-divider>
+                    </v-card-text>
+                    <v-card-actions class="pt-0">
+                        <v-btn
+                            text
+                            color="primary"
+                            class="rounded-xl"
+                        >
+                        Connect
+                        </v-btn>
+                        </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
+  const gradients = [
+    ['#222'],
+    ['#42b3f4'],
+    ['red', 'orange', 'yellow'],
+    ['purple', 'violet'],
+    ['#00c6ff', '#F0F', '#FF0'],
+    ['#f72047', '#ffd200', '#1feaea'],
+  ]
+
 
 export default ({
+    data: () => ({
+      width: 2,
+      radius: 10,
+      padding: 8,
+      lineCap: 'round',
+      gradient: gradients[5],
+      value: [13, 15, 10, 8, 4, 0, 0, 0, 3, 8, 13, 17, 17, 23, 24],
+      gradientDirection: 'top',
+      gradients,
+      fill: false,
+      type: 'trend',
+      autoLineWidth: false,
+    }),
     mounted() {
         this.$nextTick(function () {
             this.loadServerData("12321");
@@ -12,6 +91,7 @@ export default ({
     },
     methods: {
         loadServerData: function (ip) {
+            console.log(ip);
         }
     }
 })
